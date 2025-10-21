19b70ac408b466e8ec54efb20d3e48e0e170df45
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2025-10-21 13:17:00 -0400

    add MAE object

--- scripts/Prog_MAE.R
+++ scripts/Prog_MAE.R
@@ -67,3 +67,5 @@ get_MultiAssayExp <- function(study, input_dir, expr_with_counts_isoforms=FALSE)
 
 dat <- get_MultiAssayExp(study = "Obradovic", output_dir, expr_with_counts_isoforms=FALSE)
 saveRDS(dat, file=file.path(output_dir, "ICB_Obradovic.rds"))
+
+
